445f4497398aa79b64ec80a7baa13f86e3753bfd
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-01-15 23:35:52 -0500

    fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { tap } from 'rxjs';
+import { SearchService } from './pages/search/search.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'wikipedia-angular';
+  constructor(private readonly searchService: SearchService) {
+    this.searchService.search('angular').pipe(tap(console.log)).subscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,7 +8,7 @@ import { SearchModule } from './pages/search/search.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SearchModule],
+  imports: [BrowserModule, AppRoutingModule, SearchModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
