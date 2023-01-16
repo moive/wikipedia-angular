5c3853bc7287afee5715ed5f52e7ac3dd663134f
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-01-15 23:35:14 -0500

    searh submited emit

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  inputSearch = new FormControl('');
 
+  @Output() submitted = new EventEmitter<string>();
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.onChange();
+  }
+
+  onChange(): void {
+    this.inputSearch.valueChanges
+      .pipe(tap((res) => this.submitted.emit(`${res}`)))
+      .subscribe();
+  }
 }
